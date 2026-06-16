3d9f8745588e41fef14f7eaea1d50e0eda009a9b
koumedjina <koumedjinakomieric@gmail.com>
2026-06-16 22:56:56 +0000

    Premier envoi de mes codes

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+  "name": "Energy Pro App",
+  "short_name": "EnergyPro",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#eef2f7",
+  "theme_color": "#2b6cb0",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
